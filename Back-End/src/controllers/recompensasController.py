b0a7d2931a6d8249f3b89c10ccf5bb157a9d6cd8
EmilioChenf <che24841@uvg.edu.gt>
2024-05-02 17:09:45 -0600

    se realizó crud de RECOMPENSAS

--- Back-End/src/controllers/recompensasController.py
+++ Back-End/src/controllers/recompensasController.py
@@ -4,6 +4,53 @@ from config.validate import *
 from config.mongodb import mongo
 import json
     
-#funcion listar todos usuarios
 def test():
     return jsonify({'message':'Test recompensas Successfully'})
+
+# Agregar una nueva recompensa
+def add_reward():
+    data = request.get_json()
+    # Validar que los campos requeridos estén presentes
+    msg = dataRequired(data, ["id_usuario", "descripcion", "puntos", "fecha_otorgamiento"])
+    if msg:
+        return msg
+    
+    # Insertar la nueva recompensa en la base de datos
+    mongo.db.rewards.insert_one(data)
+    
+    return jsonify({'message': 'Recompensa agregada correctamente'})
+
+# Eliminar una recompensa DELETE
+def delete_reward(id):
+    # Verificar si la recompensa existe
+    reward = mongo.db.rewards.find_one({'_id': ObjectId(id)})
+    if reward is None:
+        return jsonify({'message': 'Recompensa no encontrada'})
+    
+    # Eliminar la recompensa de la base de datos
+    mongo.db.rewards.delete_one({'_id': ObjectId(id)})
+    
+    return jsonify({'message': 'Recompensa eliminada correctamente'})
+
+# Listar todas las recompensas GET
+def list_all_rewards():
+    rewards = json_util.dumps(mongo.db.rewards.find())
+    return Response(rewards, mimetype='application/json')
+
+# Listar recompensas por usuario GET
+def list_rewards_by_user(id):
+    rewards = json_util.dumps(mongo.db.rewards.find({'id_usuario': id}))
+    return Response(rewards, mimetype='application/json')
+
+# Actualizar una recompensa PUT
+def update_reward(id):
+    data = request.get_json()
+    # Verificar si la recompensa existe
+    reward = mongo.db.rewards.find_one({'_id': ObjectId(id)})
+    if reward is None:
+        return jsonify({'message': 'Recompensa no encontrada'})
+    
+    # Actualizar la recompensa en la base de datos
+    mongo.db.rewards.update_one({'_id': ObjectId(id)}, {'$set': data})
+    
+    return jsonify({'message': 'Recompensa actualizada correctamente'})
--- Back-End/src/routes/recompensasRoutes.py
+++ Back-End/src/routes/recompensasRoutes.py
@@ -5,6 +5,11 @@ from config.authenticated import ensureAuth, isAdmin
 recompensas = Blueprint('recompensas', __name__)
 
 recompensas.add_url_rule('/test', view_func=test, methods=['GET'])
+recompensas.add_url_rule('/agregar',view_func=add_reward, methods=['POST'])
+recompensas.add_url_rule('/eliminar/<id>',view_func=delete_reward, methods=['DELETE'])
+recompensas.add_url_rule('/update/<id>',view_func=update_reward,methods=['PUT'])
+recompensas.add_url_rule('/listare/<id>',view_func=list_rewards_by_user,methods=['GET'])
+recompensas.add_url_rule('/lis',view_func=list_all_rewards,methods=['GET'])
 
 ## rutas con validacion de token
 endpoints_permitidos = []
