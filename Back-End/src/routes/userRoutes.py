0c7059932aad0c5a789eae458283a3e6795296fd
her24770 <her24770@uvg.edu.gt>
2024-05-02 11:25:34 -0600

    Cambio de configuracion en token por error temporal

--- Back-End/src/routes/userRoutes.py
+++ Back-End/src/routes/userRoutes.py
@@ -17,14 +17,15 @@ endpoints_permitidos = ['users.getAll', 'users.delete', 'users.updateProfile', '
 #endpoints_permitidos = []
 @user.before_request
 def verify_token_middleware():
-    token = request.headers.get('Authorization')
-    if not token:
-        response = jsonify({"message": "Token no proporcionado"})
-        response.status_code = 401
-        return response
-    #ensureAtuh
+    # token = request.headers.get('Authorization')
+    # if not token:
+    #     response = jsonify({"message": "Token no proporcionado"})
+    #     response.status_code = 401
+    #     return response
+    # #ensureAtuh
     if request.endpoint in endpoints_permitidos:
-         return ensureAuth(token.split(" ")[1], output=False)
+         return ensureAuth(request.headers.get('Authorization').split(" ")[1], output=False)
+    
     #ADMIN
     if request.endpoint == ' ':
-        return isAdmin(token.split(" ")[1], output=False)
+        return isAdmin(request.headers.get('Authorization').split(" ")[1], output=False)
