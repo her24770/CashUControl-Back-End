64e95f97a0ac41f3abd3f82dd54f5319d4e0e845
Jriv24856 <riv24856@uvg.edu.gt>
2024-05-12 10:00:05 -0600

    Agregando funciones de like

--- Back-End/src/routes/consejosRoutes.py
+++ Back-End/src/routes/consejosRoutes.py
@@ -11,6 +11,10 @@ consejos.add_url_rule('/listAll', view_func=list_all_consejos, methods=['GET'])
 consejos.add_url_rule('/listByCategoria/<categoria>', view_func=list_ByCategoria, methods=['GET'])
 consejos.add_url_rule('/listByUser/<id>', view_func=list_ByUser, methods=['GET'])
 
+##Pa los likes 
+consejos.add_url_rule('/like/<id>', view_func=like_consejo, methods=['POST'])
+consejos.add_url_rule('/unlike/<id>', view_func=unlike_consejo, methods=['POST'])
+
 ## rutas con validacion de token
 endpoints_permitidos = ["consejos.get_consejos","consejos.delete_consejos","consejos.list_all_consejos","consejos.list_ByCategoria","consejos.list_ByUser"]
 
